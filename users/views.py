87d07be300bb46567ba439b685871e6ac8f7f3d0
Angelo <angeloalves_159@hotmail.com>
2024-12-28 14:26:09 -0300

    registro

--- users/views.py
+++ users/views.py
@@ -1,7 +1,25 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+def register(request):
+    if request.method != 'POST':
+        form = UserCreationForm()
+    
+    else:
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            
+            authenticated_user = authenticate(user=new_user.username, password = request.POST['password1'])
+            return HttpResponseRedirect(reverse('index'))
+        
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
